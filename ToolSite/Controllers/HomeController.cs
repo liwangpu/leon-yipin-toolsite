d813d1a2d100d65152b91b00ecc3e9d8c9deabdf
“liwang.pu” <liwang.pu@gmail.com>
2019-08-27 21:36:52 +0800

    1

---
+++ ToolSite/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToolSite.Models;
+
+namespace ToolSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+ 
+    }
+}
---
+++ ToolSite/Controllers/OrdersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace ToolSite.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ExtractArea()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> ExtractAreaHandle()
+        {
+            var tmpFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tmp");
+            if (!Directory.Exists(tmpFolder))
+                Directory.CreateDirectory(tmpFolder);
+            var orderFilePath = Path.Combine(tmpFolder, Guid.NewGuid().ToString() + ".xlsx");
+            var extractArea = Request.Form["area"];
+            var files = Request.Form.Files;
+
+            if (files.Count > 0)
+            {
+                using (var targetStream = System.IO.File.Create(orderFilePath))
+                    await files[0].CopyToAsync(targetStream);
+            }
+
+
+
+            return Json(new { Name = "test" });
+        }
+    }
+
+}
