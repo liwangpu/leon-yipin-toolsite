1de7cfaebe2169dea0613e5986e63e132ca12946
“liwang.pu” <liwang.pu@gmail.com>
2019-09-01 14:25:19 +0800

    1

--- ToolSite/Controllers/WarehousesController.cs
+++ ToolSite/Controllers/WarehousesController.cs
@@ -6,11 +6,24 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ToolSite.Controllers
 {
+    /// <summary>
+    /// 仓库管理控制器
+    /// </summary>
     public class WarehousesController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        /// <summary>
+        /// 库存盘点
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult StockTaking()
+        {
+            return View();
+        }
+
     }
 }
